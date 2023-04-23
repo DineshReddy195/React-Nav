b4101f39712318127eccc30ac51baeca3b1afc76
Dinesh Reddy <ndkr2716@gmail.com>
2023-04-23 11:30:54 +0530

    Assignment Completed

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <div>
+       <h3 style={{color:'white'}}> Koding Made Sample</h3>
+      </div>
+      <div className='nav-links'>
+        <a href='#' className='links'>Bootstrap</a>
+        <a href='#' className='links'>Themes</a>
+        <a href='#' className='links'>Blog</a>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
